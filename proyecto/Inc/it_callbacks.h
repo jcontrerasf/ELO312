cbd773fe39ccf6eaa8f4c7913cb074680e6a7096
jcontrerasf <50180934+jcontrerasf@users.noreply.github.com>
2020-03-23 17:46:44 -0300

    inicio proyecto

---
+++ proyecto/Inc/it_callbacks.h
@@ -0,0 +1,18 @@
+/*
+ * it_callbacks.h
+ *
+ *  Created on: 23-03-2020
+ *      Author: julio
+ */
+
+#ifndef IT_CALLBACKS_H_
+#define IT_CALLBACKS_H_
+
+#include "stdint.h"
+#include "gpio.h"
+#include "main.h"
+#include "adc.h"
+#include "tim.h"
+#include "gpio.h"
+
+#endif /* IT_CALLBACKS_H_ */
